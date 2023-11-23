b6bdba0e7e0534a86ab744a3e0d3569150db8bd9
plla2 <jhbljs92@naver.com>
2023-11-23 20:51:47 +0900

    feat: router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-function App() {
+import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
 
+function App() {
   return (
-    <div>Hello World</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
