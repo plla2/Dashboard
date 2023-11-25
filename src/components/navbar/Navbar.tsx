9e4b41f7716339b6295b88e21db2fd3bf1ffb6db
plla2 <jhbljs92@naver.com>
2023-11-25 14:56:22 +0900

    feat: navbar UI

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,28 @@
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="로고이미지" />
+        <span>Plla2</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="검색" className="icon" />
+        <img src="/app.svg" alt="앱" className="icon" />
+        <img src="/expand.svg" alt="확장" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="/man.png" alt="" />
+          <span>plla2</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
