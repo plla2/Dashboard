d2e1fd168fdf3c6d94133aad803925689b9b4202
plla2 <jhbljs92@naver.com>
2023-11-26 21:19:26 +0900

    feat: footer UI

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span className="title">Plla2</span>
+      <div className="author">ⓒPlla2 Dev Admin Dashboard</div>
+    </div>
+  );
 };
 
 export default Footer;
