95848fa4b111e91e2f8ddc41a2c69cdb8e3ac074
plla2 <jhbljs92@naver.com>
2023-12-07 15:06:20 +0900

    feat: User page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,13 @@
+import Single from "../../components/single/Single";
+import { singleUser } from "../../data";
 import "./users.scss";
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  return (
+    <div className="users">
+      <Single {...singleUser} />
+    </div>
+  );
 };
 
 export default Users;
